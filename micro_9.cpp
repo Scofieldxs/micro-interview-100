d1792df5f25162af36f1616decd55ba800b678c4
SearchVera <bjfxxs@163.com>
2014-04-28 14:59:48 +0800

    9
    输入一个整数数组，判断该数组是不是某二元查找树的后序遍历的结果。
    如果是返回true，否则返回false。
    例如输入5、7、6、9、11、10、8，由于这一整数序列是如下树的后序遍历结果，因此为true。

---
+++ micro_9.cpp
@@ -0,0 +1,64 @@
+// micro_9.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+bool isTree(int a[],int start,int end)//
+{
+	if(start==end-1) return true;
+	int root=a[end-1];
+	int midIndex;
+	
+	int i=start;
+	for(;i<end-1;i++)
+	{
+		if(a[i]>root) break;
+	}
+
+	int j;
+	for(j=i;j<end-1;j++)
+	{
+		if(a[j]<root) return false;
+	}
+
+	bool result=isTree(a,start,i);
+	if(!result) return false;
+	result=isTree(a,i,end-1);
+	if(!result) return false;
+	return true;
+}
+
+bool isTree_1(int a[],int n)
+{
+	if(a==NULL)return false;
+	if(n<=1) return true;
+	 
+	int root=a[n-1];
+	int i;
+	for(i=0;i<n-1;i++)
+	{
+		if(a[i]>root) break;
+	}
+	int j;
+	for(j=i;j<n-1;j++)
+	{
+		if(a[j]<root) return false;
+	}
+	bool result=isTree_1(a,i);
+	if(!result) return false;
+	result=isTree_1(a+i,n-1-i);
+	if(!result) return false;
+	return true;
+	
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a[]={5,7,6,9,11,10,8};
+	
+	//printf("%d",isTree(a,0,7));
+	printf("%d",isTree_1(a,7));
+	getchar();
+}
+
